64c7168641dc12628fc775e9ad65cecb763996f7
Shamwow16 <shamyleg@gmail.com>
2016-06-23 13:55:39 -0500

    Remove chart.js and clean up

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 $(function() {
 
-    /*    var Todo = Backbone.Model.extend({
-            defaults: { title: "New Todo" }
-        });
-        // Create a Firebase.Collection and set the 'firebase' property
-        // to the URL of our database
-        var TodoCollection = Backbone.Firebase.Collection.extend({
-            model: Todo,
-            url: "https://burning-inferno-5919.firebaseio.com"
-        });
-    */
+    //The code below links the calorie goal set by the user to Firebase so that anytime a user refreshes the page or comes
+    //back to the app later, the goal will be consumed from the database in Firebase instead of being re-entered. Once a goal
+    //is entered, a new ChartJS chart is created that will visually represent the number of calories a person needs to consume
+    // to reach the goal. Exceeding the calorie goal will result in a notification to the user that he/she has exceeded their goal
+    // for the day.
+
     var CalorieGoalModel = Backbone.Firebase.Model.extend({
         defaults: {
             goal: 0
@@ -19,10 +15,9 @@ $(function() {
 
     var CalorieGoals = Backbone.Firebase.Collection.extend({
         model: CalorieGoalModel,
-        /*url: "https://burning-inferno-5919.firebaseio.com"*/
+
     });
 
-    var chart;
     var CalorieGoalView = Backbone.View.extend({
         el: 'body',
         events: {
@@ -31,31 +26,12 @@ $(function() {
         initialize: function() {
             this.listenTo(this.model, 'change', this.render);
             var goal = this.model.get('goal');
-            chart = new CanvasJS.Chart("chart-container", {
-                // title: {
-                //     text: "Calories"
-                // },
-                backgroundColor: null,
-                animationEnabled: true,
-                data: [{
-                    type: "doughnut",
-                    startAngle: 20,
-                    toolTipContent: "{label}: {y}",
-                    indexLabel: "{label}",
-                    dataPoints: [
-
-                        { y: goal, label: "Calories left" },
-                        { y: 0 }
-
-                    ]
-                }]
-            });
 
         },
         render: function() {
             var self = this;
             var goal = parseInt(self.model.get('goal'));
-            self.$('#chart-container').css('visibility', 'visible');
+
             self.$('#goal-status').css('visibility', 'visible');
 
             if (logList.calorieCount < goal) {
@@ -66,26 +42,27 @@ $(function() {
                     self.$('#goal-status').html('You have reached your goal for today!');
                 } else {
                     self.$('#goal-status').html('You have exceeded your goal for the day by ' + (logList.calorieCount - goal) + ' calories');
-                    self.$('#chart-container').css('visibility', 'hidden');
-                    chart.render();
+
+
 
                     return true;
                 }
 
             }
 
-            chart.options.data[0].dataPoints[0].y = goal - logList.calorieCount;
-            chart.render();
+
         },
         setGoal: function() {
             var self = this;
             var goal = self.$('input').val();
             self.model.set('goal', goal);
             self.$('#goal-input').html(goal);
-            /* self.collection.create({ id: 0, key: goal });
-             */
         }
     });
+
+
+    //The MVP pattern for the search option has been implemented below. Any time a character is input in the search bar,
+    //
 
 
     var SearchBarModel = Backbone.Model.extend({
@@ -116,82 +93,47 @@ $(function() {
         model: SearchResult
     });
 
-    var LoggedItem = Backbone.Model.extend({
-        defaults: {
-            name: '',
-            brandName: '',
-            totalCalories: 0,
-            baseCalories: 0,
-            servingSize: 0,
-            servingUnit: '',
-            logTime: 0
-        },
-        toJSON: function() {
-            var json = Backbone.Model.prototype.toJSON.apply(this, arguments);
-            json.cid = this.cid;
-            return json;
-        }
-    });
+    var SearchBarView = Backbone.View.extend({
 
-    var LoggedList = Backbone.Firebase.Collection.extend({
-        model: LoggedItem,
-        calorieCount: 0,
-        url: "https://burning-inferno-5919.firebaseio.com"
-    });
-
-    var LoggedListView = Backbone.View.extend({
-        el: $('#log-list'),
+        el: $('#search-box'),
         events: {
-            'click #remove-icon': 'removeLogItem'
+            'keyup': 'performSearch'
         },
-        template: _.template($('#log-tmpl').html()),
         initialize: function() {
             var self = this;
-            self.listenTo(self.collection, 'add', self.render);
-            self.listenTo(self.collection, 'remove', self.render);
         },
 
-        render: function() {
-            var self = this;
-            $(self.el).find('td').empty();
+        performSearch: function() {
+            this.model.set('query', this.$el.val());
+            if (this.model.get('query') != '') {
+                this.getNutritionixData(this.model.get('query'));
+            } else {
+                $(resultList.el).html('');
+            }
+        },
+        getNutritionixData: function(query) {
+            var nutritionixUrl = "https://api.nutritionix.com/v1_1/search/" + query + "?results=0%3A10&cal_min=0&cal_max=2000&fields=nf_calories%2Cbrand_name%2Citem_name&appId=4a056a89&appKey=5aaa96859042a59ad264d3a51ff34bba";
+            $.ajax({
+                dataType: "json",
+                url: nutritionixUrl,
+                success: function(data) {
+                    searchResults.reset();
+                    var count = 0
+                    data.hits.forEach(function(hit) {
 
+                        searchResults.add(new SearchResult({ name: hit.fields.item_name, brandName: hit.fields.brand_name, baseCalories: hit.fields.nf_calories, totalCalories: hit.fields.nf_calories, servingSize: hit.fields.nf_serving_size_qty, servingUnit: hit.fields.nf_serving_size_unit }));
+                        count++
+                    });
 
-            this.collection.each(function(log) {
-                $(self.el).append(self.template(log.toJSON()));
+                    resultList.render();
+
+                }
+
             });
-            return this;
-        },
-
-        removeLogItem: function(e) {
-            e.preventDefault();
-            var element = $(e.currentTarget).parents()[1];
-            var id = $(e.currentTarget).data("id");
-            var item = this.collection.get(id);
-            this.collection.remove(item);
         }
+
     });
 
-    var totalsView = Backbone.View.extend({
-        el: $('#total-calories'),
-        initialize: function() {
-            var self = this;
-            self.listenTo(self.collection, 'add', self.addUpCalories);
-            self.listenTo(self.collection, 'remove', self.addUpCalories);
-        },
-
-        addUpCalories: function() {
-            var self = this;
-            self.collection.calorieCount = 0;
-            self.collection.each(function(log) {
-                self.collection.calorieCount += log.get('totalCalories');
-            });
-            self.$('span').html(self.collection.calorieCount);
-            chart.options.data[0].dataPoints[1] = { y: logList.calorieCount, label: "Calories Consumed" };
-            calorieGoal.render();
-        }
-    });
-
-    var resultTemplate;
     var SearchResultListView = Backbone.View.extend({
         el: $('#search-list'),
         events: {
@@ -237,7 +179,7 @@ $(function() {
             }
             var id = $(e.currentTarget).data("id");
             var item = this.collection.get(id);
-            var loggedItem = new LoggedItem({ name: item.get('name'), brandName: item.get('brandName'), baseCalories: item.get('baseCalories'), totalCalories: item.get('totalCalories'), servingSize: item.get('servingSize'), servingUnit: item.get('servingUnit'), logTime: Date.now() });
+            var loggedItem = new LoggedItem({ name: item.get('name'), brandName: item.get('brandName'), baseCalories: item.get('baseCalories'), totalCalories: item.get('totalCalories'), servingSize: item.get('servingSize'), servingUnit: item.get('servingUnit'), logTime: moment().calendar() });
             logList.create({
                 name: loggedItem.get('name'),
                 brandName: loggedItem.get('brandName'),
@@ -251,7 +193,95 @@ $(function() {
 
 
 
-    })
+    });
+
+
+
+
+    var totalsView = Backbone.View.extend({
+        el: $('#total-calories'),
+        initialize: function() {
+            var self = this;
+            self.listenTo(self.collection, 'add', self.addUpCalories);
+            self.listenTo(self.collection, 'remove', self.addUpCalories);
+        },
+
+        addUpCalories: function() {
+            var self = this;
+            self.collection.calorieCount = 0;
+            self.collection.each(function(log) {
+                self.collection.calorieCount += log.get('totalCalories');
+            });
+            self.$('span').html(self.collection.calorieCount);
+            calorieGoal.render();
+        }
+    });
+
+    var resultTemplate;
+
+
+    var LoggedItem = Backbone.Model.extend({
+        defaults: {
+            name: '',
+            brandName: '',
+            totalCalories: 0,
+            baseCalories: 0,
+            servingSize: 0,
+            servingUnit: '',
+            logTime: 0
+        },
+        toJSON: function() {
+            var json = Backbone.Model.prototype.toJSON.apply(this, arguments);
+            json.cid = this.cid;
+            return json;
+        }
+    });
+
+    var LoggedList = Backbone.Firebase.Collection.extend({
+        model: LoggedItem,
+        logDate: moment().format("D"),
+        calorieCount: 0,
+        url: "https://burning-inferno-5919.firebaseio.com"
+
+    });
+
+    var LoggedListView = Backbone.View.extend({
+        el: $('#log-list'),
+        events: {
+            'click #remove-icon': 'removeLogItem'
+        },
+        template: _.template($('#log-tmpl').html()),
+        initialize: function() {
+            var self = this;
+            self.listenTo(self.collection, 'add', self.render);
+            self.listenTo(self.collection, 'remove', self.render);
+        },
+
+        render: function() {
+            var self = this;
+            $(self.el).find('td').empty();
+
+
+            this.collection.each(function(log) {
+                $(self.el).append(self.template(log.toJSON()));
+            });
+
+            if (moment().format("D") != logList.logDate) {
+                $(this.el).find('td').empty();
+            }
+
+            return this;
+        },
+
+        removeLogItem: function(e) {
+            e.preventDefault();
+            var element = $(e.currentTarget).parents()[1];
+            var id = $(e.currentTarget).data("id");
+            var item = this.collection.get(id);
+            this.collection.remove(item);
+        }
+
+    });
 
     var goal = new CalorieGoalModel();
     var calorieGoal = new CalorieGoalView({ model: goal });
@@ -261,46 +291,7 @@ $(function() {
     var totalCalories = new totalsView({ collection: logList });
     var searchQuery = new SearchBarModel();
     var resultList = new SearchResultListView({ collection: searchResults });
-    var SearchBarView = Backbone.View.extend({
 
-        el: $('#search-box'),
-        events: {
-            'keyup': 'performSearch'
-        },
-        initialize: function() {
-            var self = this;
-        },
-
-        performSearch: function() {
-            this.model.set('query', this.$el.val());
-            if (this.model.get('query') != '') {
-                this.getNutritionixData(this.model.get('query'));
-            } else {
-                $(resultList.el).html('');
-            }
-        },
-        getNutritionixData: function(query) {
-            var nutritionixUrl = "https://api.nutritionix.com/v1_1/search/" + query + "?results=0%3A10&cal_min=0&cal_max=2000&fields=nf_calories%2Cbrand_name%2Citem_name&appId=4a056a89&appKey=5aaa96859042a59ad264d3a51ff34bba";
-            $.ajax({
-                dataType: "json",
-                url: nutritionixUrl,
-                success: function(data) {
-                    searchResults.reset();
-                    var count = 0
-                    data.hits.forEach(function(hit) {
-
-                        searchResults.add(new SearchResult({ name: hit.fields.item_name, brandName: hit.fields.brand_name, baseCalories: hit.fields.nf_calories, totalCalories: hit.fields.nf_calories, servingSize: hit.fields.nf_serving_size_qty, servingUnit: hit.fields.nf_serving_size_unit }));
-                        count++
-                    });
-
-                    resultList.render();
-
-                }
-
-            });
-        }
-
-    });
 
 
     var searchBar = new SearchBarView({ model: searchQuery });
